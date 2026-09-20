b8e3a34b8785ce17de2c25f6f083c5629f932733
Mateusz Gegotek <mat.g.890517@gmail.com>
2026-09-20 05:15:48 -0700

    Revert "Update main.py"

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ import time
 TEXT_FILE_PATH = "text.txt"
 
 COAUTHOR_1 = "Co-authored-by: matg890517-art <mat.g.890517@gmail.com>"
-COAUTHOR_2 = "Co-authored-by: anthonycastro0405 <tom.tuttle.1008@gmail.com>"
+COAUTHOR_2 = "Co-authored-by: anthonycastro0405 <second-co-author-yukitanaka7070@gmail.com>"
 
 COMMIT_MESSAGE = f"""
 Added a small change to {TEXT_FILE_PATH}
